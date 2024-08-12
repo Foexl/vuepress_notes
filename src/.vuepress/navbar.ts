8dd7e44dcc43af4187de537fd343fcc1242d8c6f
YL <14767035@qq.com>
2024-08-12 23:10:02 +0800

    add searchPro、icon

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -5,9 +5,11 @@ export default navbar([
   {
     text: "python",
     link: "/python/",
+    icon: "/icon/Python.svg",
   },
   {
     text: "数据库",
     link: "/数据库/",
+    icon: "/icon/云数据库.svg",
   },
 ]);
